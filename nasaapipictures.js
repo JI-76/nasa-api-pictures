170ac62c51c1f67c48b219321dd04b044bd3024b
JI-76 <johnison76@hotmail.com>
2021-01-27 15:41:13 -0800

    Javascript - NASA API DocFetch

--- nasaapipictures.js
+++ nasaapipictures.js
@@ -1,2 +1,24 @@
 // nasaapipictures.js
 
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+// global array to store fetch results
+let resultsArray = [];
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+    try {
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+        console.log(resultsArray);
+    } catch (error) {
+        // Catch error here
+        console.log(error);
+    };
+};
+
+// On Load
+getNasaPictures();
